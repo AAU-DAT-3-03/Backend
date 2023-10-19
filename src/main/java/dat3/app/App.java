a7ffa1a835e43f09e0f47a747c1146dafcc85d9e
madsbyriel <madshbyriel@gmail.com>
2023-10-19 07:32:24 +0200

    Now the App class runs server

--- src/main/java/dat3/app/App.java
+++ src/main/java/dat3/app/App.java
@@ -1,19 +1,33 @@
 package dat3.app;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.io.IOException;
+
+import com.sun.net.httpserver.HttpExchange;
+
+import dat3.app.classes.Server;
+
 public class App {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        // Server need this address and port, so pleaso dont change it.
+        Server server = new Server("10.92.0.231", 80);
+        server.addGetRoute("/", App::testIfItWorksIndex);
+        try {
+            server.startServer();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
+
+    private static void testIfItWorksIndex(HttpExchange exchange) {
+        String response = "Hello from Index!";
+        try {
+            exchange.sendResponseHeaders(200, response.length());
+            exchange.getResponseBody().write(response.getBytes());
+            exchange.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        exchange.close();
     }
 }
