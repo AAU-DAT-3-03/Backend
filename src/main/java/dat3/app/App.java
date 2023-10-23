542648d9ed8f9c914fcc3149cfb7b6c3121635c0
madsbyriel <madshbyriel@gmail.com>
2023-10-23 08:56:41 +0200

    Now reading port and host from json file.

--- src/main/java/dat3/app/App.java
+++ src/main/java/dat3/app/App.java
@@ -1,15 +1,37 @@
 package dat3.app;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonSyntaxException;
 import com.sun.net.httpserver.HttpExchange;
 
 import dat3.app.classes.Server;
 
 public class App {
     public static void main(String[] args) {
-        // Server need this address and port, so pleaso dont change it.
-        Server server = new Server("10.92.0.231", 80);
+        ServerInfo serverInfo;
+        try {
+            Gson gson = new Gson();
+            serverInfo = gson.fromJson(
+                    getProjectSettingsReader(), // Arg1 = filestream 
+                    ServerInfo.class // Arg2 = class info
+                );
+        } catch (FileNotFoundException e) {
+            System.out.println("'./projectsettings.json' was not found. Make sure to include it in the project root directory.");
+            return;
+        } catch (JsonIOException | JsonSyntaxException e) {
+            e.printStackTrace();
+            System.out.println("Failed parsing the projectsettings.");
+            return;
+        }
+
+        Server server = new Server(serverInfo.getHostname(), serverInfo.getPort());
         server.addGetRoute("/", App::testIfItWorksIndex);
         try {
             server.startServer();
@@ -29,5 +51,21 @@ public class App {
         }
 
         exchange.close();
+    }
+
+    public static Reader getProjectSettingsReader() throws FileNotFoundException {
+        return new FileReader(new File(System.getProperty("user.dir") + "/projectsettings.json"));
+    }
+}
+
+class ServerInfo {
+    private String hostname;
+    private int port;
+
+    public String getHostname() {
+        return hostname;
+    }
+    public int getPort() {
+        return port;
     }
 }
