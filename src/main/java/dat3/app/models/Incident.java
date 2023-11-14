5263c32c0b1aa653091fea96ce6c894ccffbe344
Mads Rohde Guldbæk <mguldb22@student.aau.dk>
2023-11-14 17:22:16 +0100

    purged old classes, moved event and incident class to models

--- src/main/java/dat3/app/models/Incident.java
+++ src/main/java/dat3/app/models/Incident.java
@@ -1,4 +1,4 @@
-package dat3.app.classes;
+package dat3.app.models;
 
 import com.google.gson.Gson;
 import com.mongodb.client.MongoClients;
@@ -11,7 +11,7 @@ import org.bson.types.ObjectId;
 import java.util.ArrayList;
 import java.util.Date;
 
-public class Incident {
+public class Incident extends StandardModel<Incident> {
     private int priority = 0;
     private boolean acknowledged = false;
     private ObjectId acknowledgedBy;
@@ -40,34 +40,34 @@ public class Incident {
         return this._id;
     }
 
-    public Worker GetAcknowledgedBy() {
+    public User GetAcknowledgedBy() {
         ProjectSettings settings = ProjectSettings.getProjectSettings();
         MongoDatabase db = MongoClients.create(settings.getDbConnectionString()).getDatabase(settings.getDbName());
-        MongoCollection<Document> workerCollection = db.getCollection("user");
+        MongoCollection<Document> userCollection = db.getCollection("user");
         Document filter = new Document();
         filter.put("ObjectID", this.acknowledgedBy);
-        Document workerDocument = workerCollection.find(filter).first();
-        return new Gson().fromJson(workerDocument.toJson(), Worker.class);
+        Document userDocument = userCollection.find(filter).first();
+        return new Gson().fromJson(userDocument.toJson(), User.class);
     }
 
-    public ArrayList<Worker> GetWorkers() {
-        ArrayList<Worker> workerList = new ArrayList<Worker>();
+    public ArrayList<User> GetUsers() {
+        ArrayList<User> userList = new ArrayList<>();
         ProjectSettings settings = ProjectSettings.getProjectSettings();
         MongoDatabase db = MongoClients.create(settings.getDbConnectionString()).getDatabase(settings.getDbName());
-        MongoCollection<Document> workerCollection = db.getCollection("user");
+        MongoCollection<Document> userCollection = db.getCollection("user");
 
-        for (ObjectId workerID: this.users) {
+        for (ObjectId userID: this.users) {
             Document filter = new Document();
-            filter.put("ObjectID", workerID);
-            Document workerDocument = workerCollection.find(filter).first();
-            workerList.add(new Gson().fromJson(workerDocument.toJson(), Worker.class));
+            filter.put("ObjectID", userID);
+            Document userDocument = userCollection.find(filter).first();
+            userList.add(new Gson().fromJson(userDocument.toJson(), User.class));
         }
 
-        return workerList;
+        return userList;
     }
 
     public ArrayList<Alarm> GetAlarms() {
-        ArrayList<Alarm> alarmList = new ArrayList<Alarm>();
+        ArrayList<Alarm> alarmList = new ArrayList<>();
         ProjectSettings settings = ProjectSettings.getProjectSettings();
         MongoDatabase db = MongoClients.create(settings.getDbConnectionString()).getDatabase(settings.getDbName());
         MongoCollection<Document> alarmCollection = db.getCollection("Alarm");
@@ -83,7 +83,7 @@ public class Incident {
     }
 
     public ArrayList<Event> GetEventLog() {
-        ArrayList<Event> eventList = new ArrayList<Event>();
+        ArrayList<Event> eventList = new ArrayList<>();
         ProjectSettings settings = ProjectSettings.getProjectSettings();
         MongoDatabase db = MongoClients.create(settings.getDbConnectionString()).getDatabase(settings.getDbName());
         MongoCollection<Document> eventCollection = db.getCollection("event");
@@ -97,15 +97,32 @@ public class Incident {
 
         return eventList;
     }
+    @Override
+    public Document toDocument(){
+        Document document = new Document();
+        document.append("Priority", this.priority);
+        document.append("Acknowledged", this.acknowledged);
+        if (this.acknowledgedBy != null) document.append("AcknowledgedBy", this.acknowledged);
+        if (this.creationDate != null) document.append("Date", this.creationDate);
+        if (this._id != null) document.append("ID", this._id);
+        if (this.users != null) document.append("Users", this.users);
+        if (this.alarms != null) document.append("Alarms", this.alarms);
+        if (this.eventLog!= null) document.append("Eventlog", this.eventLog);
+        return document;
+    }
+    @Override
+    public Incident fromDocument(Document document) {
+        return new Incident("dsa");
+    }
 
-    public Document ToDocument(){
+    public Document toDocumentFormatted() {
         Document document = new Document();
         document.append("Priority", this.priority);
         document.append("Acknowledged", this.acknowledged);
         if (this.acknowledgedBy != null) document.append("AcknowledgedBy", this.GetAcknowledgedBy());
         if (this.creationDate != null) document.append("Date", this.creationDate);
         if (this._id != null) document.append("ID", this._id);
-        if (this.users != null) document.append("Users", this.GetWorkers());
+        if (this.users != null) document.append("Users", this.GetUsers());
         if (this.alarms != null) document.append("Alarms", this.GetAlarms());
         if (this.eventLog!= null) document.append("Eventlog", this.GetEventLog());
         return document;
