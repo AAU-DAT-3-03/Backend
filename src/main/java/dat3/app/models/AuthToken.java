4758ff790bad14b632fa65443d372116a83a30ad
madsbyriel <madshbyriel@gmail.com>
2023-11-14 13:47:49 +0100

    Major model  refactoring

--- src/main/java/dat3/app/models/AuthToken.java
+++ src/main/java/dat3/app/models/AuthToken.java
@@ -4,14 +4,10 @@ import org.bson.Document;
 import org.bson.types.ObjectId;
 
 import com.mongodb.client.ClientSession;
-import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoIterable;
-import com.mongodb.client.result.DeleteResult;
-import com.mongodb.client.result.InsertOneResult;
 import com.mongodb.client.result.UpdateResult;
 
-public class AuthToken extends Model<AuthToken> {
+public class AuthToken extends StandardModel<AuthToken> {
     private ObjectId _id = null;
     private String name = null;
     private Long expiryDate = null;
@@ -86,106 +82,6 @@ public class AuthToken extends Model<AuthToken> {
     }
 
     // ---------- Abstract method overrides ---------- //
-    @Override
-    public InsertOneResult insertOne(MongoCollection<Document> collection) throws Exception {
-        Document toInsert = this.toDocument();
-        return collection.insertOne(toInsert);
-    }
-
-    @Override
-    public InsertOneResult insertOne(MongoCollection<Document> collection, ClientSession session) throws Exception {
-        Document toInsert = this.toDocument();
-        return collection.insertOne(session, toInsert);
-    }
-
-    @Override
-    public AuthToken findOne(MongoCollection<Document> collection) throws Exception {
-        Document filter = this.toDocument();
-        Document result = collection.find(filter).first();
-        if (result == null) return null;
-        return this.fromDocument(result);
-    }
-
-    @Override
-    public AuthToken findOne(MongoCollection<Document> collection, ClientSession session) throws Exception {
-        Document filter = this.toDocument();
-        Document result = collection.find(session, filter).first();
-        if (result == null) return null;
-        return this.fromDocument(result);
-    }
-
-    @Override
-    public MongoIterable<AuthToken> findMany(MongoCollection<Document> collection) throws Exception {
-        Document filter = this.toDocument();
-        FindIterable<Document> result = collection.find(filter);
-        MongoIterable<AuthToken> mapped = result.map((Document doc) -> {
-            try  {
-                return this.fromDocument(doc);
-            } catch (Exception e) {
-                return null;
-            }
-        });
-        return mapped;
-    }
-
-    @Override
-    public MongoIterable<AuthToken> findMany(MongoCollection<Document> collection, ClientSession session) throws Exception {
-        Document filter = this.toDocument();
-        FindIterable<Document> result = collection.find(session, filter);
-        MongoIterable<AuthToken> mapped = result.map((Document doc) -> {
-            try  {
-                return this.fromDocument(doc);
-            } catch (Exception e) {
-                return null;
-            }
-        });
-        return mapped;
-    }
-
-    @Override
-    public UpdateResult updateOne(MongoCollection<Document> collection, AuthToken filter) throws Exception {
-        Document setOperation = new Document("$set", this.toDocument());
-        return collection.updateOne(filter.toDocument(), setOperation);
-    }
-
-    @Override
-    public UpdateResult updateOne(MongoCollection<Document> collection, ClientSession session, AuthToken filter) throws Exception {
-        Document setOperation = new Document("$set", this.toDocument());
-        return collection.updateOne(session, filter.toDocument(), setOperation);
-    }
-
-    @Override
-    public UpdateResult updateMany(MongoCollection<Document> collection, AuthToken filter) throws Exception {
-        Document setOperation = new Document("$set", this.toDocument());
-        return collection.updateMany(filter.toDocument(), setOperation);
-    }
-
-    @Override
-    public UpdateResult updateMany(MongoCollection<Document> collection, ClientSession session, AuthToken filter) throws Exception {
-        Document setOperation = new Document("$set", this.toDocument());
-        return collection.updateMany(session, filter.toDocument(), setOperation);
-    }
-
-    @Override
-    public DeleteResult deleteOne(MongoCollection<Document> collection) throws Exception {
-        return collection.deleteOne(this.toDocument());
-    }
-
-    @Override
-    public DeleteResult deleteOne(MongoCollection<Document> collection, ClientSession session) throws Exception {
-        return collection.deleteOne(session, this.toDocument());
-    }
-
-    @Override
-    public DeleteResult deleteMany(MongoCollection<Document> collection) throws Exception {
-        return collection.deleteMany(this.toDocument());
-    }
-
-    @Override
-    public DeleteResult deleteMany(MongoCollection<Document> collection, ClientSession session) throws Exception {
-        return collection.deleteMany(session, this.toDocument());
-    }
-
     @Override
     public Document toDocument() {
         Document document = new Document();
--- src/main/java/dat3/app/models/Model.java
+++ src/main/java/dat3/app/models/Model.java
@@ -11,25 +11,18 @@ import com.mongodb.client.result.UpdateResult;
 
 abstract class Model <T> {
     // Insertion operation
-    public abstract InsertOneResult insertOne(MongoCollection<Document> collection) throws Exception;
     public abstract InsertOneResult insertOne(MongoCollection<Document> collection, ClientSession session) throws Exception;
 
     // Find operations
-    public abstract T findOne(MongoCollection<Document> collection) throws Exception;
     public abstract T findOne(MongoCollection<Document> collection, ClientSession session) throws Exception;
-    public abstract MongoIterable<T> findMany(MongoCollection<Document> collection) throws Exception;
     public abstract MongoIterable<T> findMany(MongoCollection<Document> collection, ClientSession session) throws Exception;
 
     // Update operations
-    public abstract UpdateResult updateOne(MongoCollection<Document> collection, T filter) throws Exception;
-    public abstract UpdateResult updateOne(MongoCollection<Document> collection, ClientSession session, T filter) throws Exception;
-    public abstract UpdateResult updateMany(MongoCollection<Document> collection, T filter) throws Exception;
-    public abstract UpdateResult updateMany(MongoCollection<Document> collection, ClientSession session, T filter) throws Exception;
+    public abstract <U extends Model<U>> UpdateResult updateOne(MongoCollection<Document> collection, ClientSession session, U filter) throws Exception;
+    public abstract <U extends Model<U>> UpdateResult updateMany(MongoCollection<Document> collection, ClientSession session, U filter) throws Exception;
 
     // Delete operations
-    public abstract DeleteResult deleteOne(MongoCollection<Document> collection) throws Exception;
     public abstract DeleteResult deleteOne(MongoCollection<Document> collection, ClientSession session) throws Exception;
-    public abstract DeleteResult deleteMany(MongoCollection<Document> collection) throws Exception;
     public abstract DeleteResult deleteMany(MongoCollection<Document> collection, ClientSession session) throws Exception;
 
     // Parsing
